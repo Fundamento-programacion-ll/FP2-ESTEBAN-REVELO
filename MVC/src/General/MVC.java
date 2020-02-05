9acec773b4fc97ec478e9085289ef3f6e189b0a3
StbanJ <59964568+StbanJ@users.noreply.github.com>
2020-02-05 07:59:47 -0600

    actualización proyectos

--- MVC/src/General/MVC.java
+++ MVC/src/General/MVC.java
@@ -7,7 +7,7 @@ public class MVC {
 
     public static void main(String[] args) {
         Usuario inicio = new Usuario();
-        Persona Bryan = new Persona("Bryan", "Flores", "1724440092", 20, "5112608", "Comite del Pueblo", 'M', false);
+        Persona Bryan = new Persona("Esteban", "Revelo", "1003982269", 20, "2444444", "Llano Grande", 'M', false);
         System.out.println(Bryan);
         inicio.menu();
     }
--- MVC/src/Vista/Usuario.java
+++ MVC/src/Vista/Usuario.java
@@ -9,7 +9,7 @@ public class Usuario {
     public void menu() {
         User metodo = new User();
         int opcion = 0;
-        opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "MENU \n" + "1. Agregar Persona \n" + "2. Listar Personas \n" + "3. Salir \n", this));
+        opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "MENU \n" + "1. Agregar Persona \n" + "2. Listar Personas \n" + "3. SALIR \n", this));
         switch (opcion) {
             case 1:
                 String nombre = JOptionPane.showInputDialog(null, "INGRESE EL NOMBRE", this);
@@ -19,10 +19,10 @@ public class Usuario {
                 System.out.println(metodo.listarPersonas());
                 break;
             case 3:
-                System.out.println("Ejecucuón Finalizada");
+                System.out.println("EJECUCION FINALIZADA");
                 break;
             default:
-                System.out.println("Ejecucuón Finalizada");
+                System.out.println("EJECUCION FINALIZADA");
                 break;
         }
     }
